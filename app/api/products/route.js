c10eb6d9bb745d9a7361f0c7a92d88f98056cc69
nairx <nairxprv@gmail.com>
2026-04-06 17:04:19 +0530

    updated

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -1,3 +1,5 @@
+
+
 export async function GET() {
   const products = [
     { id: 1, name: "Product 1", price: 100 },
--- app/page.js
+++ app/page.js
@@ -1,10 +1,18 @@
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
-  return <div>Hello World
-    <p>
-      <Link href="/about">About Page</Link>
-    </p>
-  </div>;
+export default async function Home() {
+  const res = await fetch("http://localhost:3000/api/products");
+  const products = await res.json();
+  return (
+    <div>
+      Hello World
+      <p>
+        <Link href="/about">About Page</Link>
+      </p>
+      {products.map((product) => (
+        <li>{product.name}</li>
+      ))}
+    </div>
+  );
 }
