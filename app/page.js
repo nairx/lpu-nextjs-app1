09301a9f687d5cd08872a43e98f1b715c5e71acd
nairx <nairxprv@gmail.com>
2026-04-06 17:27:33 +0530

    updated

--- app/page.js
+++ app/page.js
@@ -1,18 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
 export default async function Home() {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
-  const res = await fetch(`${baseUrl}/api/products`);
-  const products = await res.json();
   return (
     <div>
       Hello World
       <p>
         <Link href="/about">About Page</Link>
       </p>
-      {products.map((product) => (
-        <li>{product.name}</li>
-      ))}
     </div>
   );
 }
